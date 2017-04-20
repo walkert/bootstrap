251a0d15d7a47a3396db01585ac1020cb2830b44
Tony Walker <tonywalker.uk@gmail.com>
2017-04-20 13:49:04 -0400

    Check required packages exist before we start

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,31 @@ PY_DIR="$(python -m site --user-base)/bin"
 LOCAL_PIP=$(which pip 2>/dev/null)
 export PATH=${PY_DIR}:$PATH
 
+# Check pre-reqs are installed
+SHARED_REQS="make fontconfig unzip"
+if [ -e /etc/redhat-release ] ; then
+    PKG="rpm -q"
+    REQS="${SHARED_REQS} python-devel openssl-devel ncurses-devel libevent-devel"
+else
+    PKG="dpkg -L"
+    REQS="${SHARED_REQS} python-dev libssl-dev libncurses5-dev libevent-dev"
+fi
+
+if ! $PKG cc &>/dev/null && ! $PKG gcc &>/dev/null ; then
+    echo "You must install a compiler"
+    exit 1
+fi
+quit=0
+for req in $REQS ; do
+    if ! $PKG $req &>/dev/null ; then
+        quit=1
+        echo "You must install $req"
+    fi
+done
+if [ $quit -eq 1 ] ; then
+    exit 1
+fi
+
 # Get pip if required
 if [ -z "$LOCAL_PIP" ] ; then
     LOCAL_PIP="${PY_DIR}/pip"
