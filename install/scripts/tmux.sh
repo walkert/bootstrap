c0cd6ad133414d67cf066ec0a74a337dba009d04
Tony Walker <walkert.uk@gmail.com>
2019-07-29 10:55:36 -0400

    Use brew-based tmux installation

--- install/scripts/tmux.sh
+++ install/scripts/tmux.sh
@@ -6,8 +6,6 @@
 . ${1}/vars.sh
 . ${1}/common.sh
 
-tmux_dir="${binaries_dir}/tmux"
-tmux=("${tmux_dir}/bin/tmux")
 tmux_scripts_dest="${HOME}/.tmux"
 tmux_scripts_source="$(dirname $1)/tmux"
 
@@ -29,46 +27,15 @@ install_plugins(){
 }
 
 install_tmux(){
-    if is_mac ; then
-        brew_install tmux
-        return
-    fi
-    if is_redhat ; then
-        install_pkg "${tmux_reqs_red[@]}"
-    else
-        install_pkg "${tmux_reqs_deb[@]}"
-    fi
-    local tfile="tmux.tgz"
-    local dltemp=$(mktemp -d)
-    run "wget -O $tfile ${tmux_repo_base}/${tmux_version}/${tmux_tar_name}"
-    run "tar -C $dltemp --strip 1 -xzf $tfile"
-    cd $dltemp
-    run "./configure --prefix=$tmux_dir"
-    run "make"
-    run "make install"
-    cd - &>/dev/null
-    run "rm -rf $tfile $dltemp"
-    ensure_link ${tmux} ${bin_dir}/tmux
+    for pkg in "${tmux_brew_packages[@]}" ; do
+        brew_install $pkg
+    done
 }
 
 
 if [ ! -L $tmux_scripts_dest ] ; then
     ensure_link $tmux_scripts_source $tmux_scripts_dest
 fi
-installed=$(which tmux 2>/dev/null)
-if [ -n "$installed" ] ; then
-    tmux+=($installed)
-fi
-for binary in ${tmux[*]} ; do
-    if [ ! -e $binary ] ; then
-        continue
-    fi
-    if $binary -V | grep -q $tmux_version ; then
-        install_plugins
-        exit
-    fi
-done
-install_devel
 echo "Installing tmux.."
 install_tmux
 install_plugins
