522fcf8f4503e7eac83157c6c4eba2b0f5cb1677
Tony Walker <walkert.uk@gmail.com>
2020-09-11 16:26:11 -0400

    Switch to tmux-thumbs and improve setup
    It seems plugins are automagically cloned during start/setup.

--- install/scripts/tmux.sh
+++ install/scripts/tmux.sh
@@ -9,33 +9,13 @@
 tmux_scripts_dest="${HOME}/.tmux"
 tmux_scripts_source="$(dirname $1)/tmux"
 
-install_plugins(){
-    plug_dir="${tmux_scripts_source}/plugins"
-    ensure_dir $plug_dir
-    for plugin in "${tmux_plugins[@]}" ; do
-        plug_name=$(basename $plugin)
-        plug_dest=${plug_dir}/$plug_name
-        if [ -d $plug_dest ] ; then
-            continue
-        fi
-        run "git clone $plugin $plug_dest"
-        if [ $plug_name = "tmux-fingers" ] ; then
-            cd ${plug_dest}
-            run "git submodule update --init --recursive"
-        fi
-    done
-}
-
 install_tmux(){
-    echo "Installing tmux.."
     for pkg in "${tmux_brew_packages[@]}" ; do
         brew_install $pkg
     done
 }
 
-
 if [ ! -L $tmux_scripts_dest ] ; then
     ensure_link $tmux_scripts_source $tmux_scripts_dest
 fi
 install_tmux
-install_plugins
