909deec3c1cb70335f3528b84e670f18d065ae4b
Tony Walker <walkert.uk@gmail.com>
2022-05-10 10:15:50 +0100

    Reorder options.lua
     - Remove superfluous filetype plugin line
     - Add listchars

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,27 +1,27 @@
 local set = vim.opt
 
 vim.cmd "filetype plugin indent on"
-vim.cmd "filetype plugin on"
 
 set.autoindent = true
+set.autoread = true
+set.backspace = {"indent", "eol", "start"}
+set.completeopt = "menu,menuone,noselect"
 set.cursorline = true
 set.expandtab = true
-set.tabstop = 4
-set.shiftwidth = 4
-set.shiftround = true
-set.softtabstop = 4
-set.ignorecase = true
-set.smartcase = true
-set.scrolloff = 3
-set.backspace = {"indent", "eol", "start"}
 set.formatoptions = set.formatoptions + 'ro'
-set.modelines = 0
 set.hlsearch = true
-set.splitright = true
-set.splitbelow = true
+set.ignorecase = true
+set.listchars = "eol:$,tab:>-"
+set.modelines = 0
 set.number = true
 set.relativenumber = true
-set.autoread = true
+set.scrolloff = 3
+set.shiftround = true
+set.shiftwidth = 4
 set.showtabline = 1
 set.signcolumn = "number"
-set.completeopt = "menu,menuone,noselect"
+set.smartcase = true
+set.softtabstop = 4
+set.splitbelow = true
+set.splitright = true
+set.tabstop = 4
