fc31b258acbf3b9e40dba27eef2493dd7e36b683
Tony Walker <walkert.uk@gmail.com>
2022-11-08 13:28:48 +0000

    Track upstream changes for rust-tools

--- config/nvim/lua/config/rust-tools.lua
+++ config/nvim/lua/config/rust-tools.lua
@@ -1,11 +1,5 @@
 local on_attach = require('config/utils').on_attach
 local opts = {
-    tools = {
-        inlay_hints = {
-            only_current_line = true,
-            only_current_line_autocmd = "CursorMoved,CursorMovedI",
-        }
-    },
     server = {
         on_attach = on_attach,
         settings = {
