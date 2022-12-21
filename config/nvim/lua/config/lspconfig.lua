b8fb1e5a7dc3dfe9d9bb9ba30d14688ed6778a22
Tony Walker <walkert.uk@gmail.com>
2022-12-21 10:39:17 +0000

    Add sumneko_lua to lspconfig

--- config/nvim/lua/config/lspconfig.lua
+++ config/nvim/lua/config/lspconfig.lua
@@ -6,7 +6,7 @@ local on_attach = require('config/utils').on_attach
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local servers = { 'gopls', 'pyright', 'terraformls' } -- rust_analyzer is configured by rust-tools
+local servers = { 'gopls', 'pyright', 'sumneko_lua', 'terraformls' } -- rust_analyzer is configured by rust-tools
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
@@ -40,4 +40,28 @@ require "lspconfig".efm.setup {
     },
     filetypes = { 'python' },
     root_dir = require("lspconfig").util.root_pattern{".git/", "."},
+}
+-- Lua setup
+require "lspconfig".sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
