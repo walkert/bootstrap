fc0c55e62f971458eeddc1dce95cff71e5f0be75
Tony Walker <walkert.uk@gmail.com>
2023-01-26 11:08:00 +0000

    Add incremental_selection to treesitter

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -15,6 +15,15 @@ require'nvim-treesitter.configs'.setup {
   rainbow = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
   -- nvim-treesitter-textobjects config
   textobjects = {
     select = {
