6aef7a9dc877af801032a6df3078b7a734d912bb
Tony Walker <walkert.uk@gmail.com>
2023-06-05 17:18:58 +0100

    Fix Comment keymap

--- config/nvim/lua/config/Comment.lua
+++ config/nvim/lua/config/Comment.lua
@@ -1,4 +1,4 @@
 require('Comment').setup()
 -- Set these using nvim_set_keymap as they don't work with keymap.set
 vim.api.nvim_set_keymap('n', '<leader>c', "gcc", { silent = true })
-vim.api.nvim_set_keymap('v', '<leader>c', "gcc", { silent = true })
+vim.api.nvim_set_keymap('v', '<leader>c', "gc", { silent = true })
