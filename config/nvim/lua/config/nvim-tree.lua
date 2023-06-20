e63dccf3a8d2f67c82214dc92c36e4d2dc14d29a
Tony Walker <walkert.uk@gmail.com>
2023-06-20 13:49:37 +0100

    Migrate to nvim-tree on_attach for mappings

--- config/nvim/lua/config/nvim-tree.lua
+++ config/nvim/lua/config/nvim-tree.lua
@@ -1,19 +1,26 @@
 local nmap = require("config/utils").nmap
 nmap('<C-t>', ":NvimTreeToggle<CR>", "Toggle NvimTree")
 
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 require'nvim-tree'.setup {
     actions = {
         open_file = {
             quit_on_open = true
         }
     },
-    view = {
-        mappings = {
-            list = {
-                { key = {"t"}, cb = tree_cb("tabnew") },
-                { key = {"<C-t>"}, cb = tree_cb("close") },
-            }
+    on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+        return {
+            desc = 'nvim-tree: ' .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
         }
-    }
+        end
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set('n', 't', api.node.open.tab, opts('Open: New Tab'))
+        vim.keymap.set('n', '<C-t>', api.tree.close, opts('Close'))
+    end
 }
