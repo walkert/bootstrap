5467e5028e127b943ccc6f078552013b5b32b41f
Tony Walker <walkert.uk@gmail.com>
2023-08-02 15:42:16 +0100

    Add new keymaps to fzf/nvim-surround

--- config/nvim/lua/config/fzf.lua
+++ config/nvim/lua/config/fzf.lua
@@ -9,6 +9,8 @@ nmap('<leader>sf', "<cmd>lua require('fzf-lua').files()<CR>", "[S]earch  [F]iles
 nmap('<leader>sh', "<cmd>lua require('fzf-lua').help_tags()<CR>", "[S]earch  [H]elp")
 nmap('<leader>sk', "<cmd>lua require('fzf-lua').keymaps()<CR>", "[S]earch [K]eymaps")
 nmap('<leader>sm', "<cmd>lua require('fzf-lua').marks()<CR>", "[S]earch [M]arks")
+nmap('<leader>sgc', "<cmd>lua require('fzf-lua').git_commits()<CR>", "[S]earch  [G]it [C]ommits")
+nmap('<leader>sgb', "<cmd>lua require('fzf-lua').git_bcommits()<CR>", "[S]earch  [G]it [B]uffer Commits")
 
 local actions = require "fzf-lua.actions"
 require'fzf-lua'.setup {
--- config/nvim/lua/config/nvim-surround.lua
+++ config/nvim/lua/config/nvim-surround.lua
@@ -1,3 +1,4 @@
 require("nvim-surround").setup()
 -- Must set using nvim_set_keymap for some reason. vim.keymap.set doesn't work.
-vim.api.nvim_set_keymap('n', '<leader>s', 'ysiw', { noremap = false, silent = true, desc = "Set Surround" })
+vim.api.nvim_set_keymap('n', '<leader>s', 'ysiw', { noremap = false, silent = true, desc = "Set Surround around word" })
+vim.api.nvim_set_keymap('n', '<leader>S', 'ysiW', { noremap = false, silent = true, desc = "Set Surround around WORD" })
