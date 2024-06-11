35b5269784cfba43ca15ab3a81738b963ca86208
Tony Walker <walkert.uk@gmail.com>
2024-06-11 14:16:51 +0100

    Disable snippets inside comments for cmp

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -4,6 +4,17 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 cmp.setup({
+    enabled = function()
+      -- disable completion in comments
+      local context = require 'cmp.config.context'
+      -- keep command mode completion enabled when cursor is in a comment
+      if vim.api.nvim_get_mode().mode == 'c' then
+        return true
+      else
+        return not context.in_treesitter_capture("comment")
+          and not context.in_syntax_group("Comment")
+      end
+    end,
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
