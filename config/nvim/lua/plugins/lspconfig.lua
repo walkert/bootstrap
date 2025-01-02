ff417b583eccf6b35eb300b5b192d96e81f35c94
Tony Walker <walkert.uk@gmail.com>
2025-01-02 15:19:54 +0000

    Add new Ruff ignores and re-enable pyright checks

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -132,8 +132,10 @@ return {
                     },
                     python = {
                         analysis = {
-                            -- Ignore all files for analysis to exclusively use Ruff for linting
-                            ignore = { '*' },
+                            diagnosticSeverityOverrides = {
+                                reportAttributeAccessIssue = false,
+                                reportOptionalMemberAccess = false
+                            }
                         },
                     },
                 },
